2b8ddaf857629175e4c091f4821c04916e06231f
michelleandersson <michelleandersson@live.com>
2020-05-29 15:36:23 +0200

    start work on api search

--- src/main.js
+++ src/main.js
@@ -11,4 +11,20 @@ const app = new Vue({
         this.topSongs = topSongsResponce.message.body.track_list; 
       })
     } 
-  }) 
+  }); 
+
+  const app2 = new Vue({
+    el: '#find-song', 
+    data: {
+      findSongs: [], 
+      //searchResult: , 
+    },
+    // Mounted hook is being called after the app is fully initialized
+    mounted: function() {
+      fetch(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track_artist=cher&page_size=10&page=1&s_track_rating=desc&apikey=e06708be7a728768734c486cd6c6547e`)
+      .then(response => response.json())
+      .then(findSongsResponce => {
+        this.findSongs = findSongsResponce.message.body.track_list; 
+      })
+    } 
+  }); 
