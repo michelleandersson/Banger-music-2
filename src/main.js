543170e821011b9cb6a94dea3ec0bf2266228029
michelleandersson <michelleandersson@live.com>
2020-05-30 15:03:10 +0200

    clean code

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ const app = new Vue({
     },
     // Mounted hook is being called after the app is fully initialized
     mounted: function() {
-      fetch('https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=9&country=us&f_has_lyrics=1&apikey=e06708be7a728768734c486cd6c6547e')
+      fetch('https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=e06708be7a728768734c486cd6c6547e')
       .then(response => response.json())
       .then(topSongsResponce => {
         this.topSongs = topSongsResponce.message.body.track_list; 
@@ -27,7 +27,6 @@ const app = new Vue({
     el: '#find-song', 
     data: {
       findSongs: [], 
-      //searchResult: , 
       songSearch: ''
     },
     methods: {
@@ -39,14 +38,6 @@ const app = new Vue({
       })
 
       }
-    },
-    // Mounted hook is being called after the app is fully initialized
-    /*mounted: function() {
-      fetch('https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track_artist='+this.songSearch+'&page_size=10&page=1&s_track_rating=desc&apikey=e06708be7a728768734c486cd6c6547e')
-      .then(response => response.json())
-      .then(findSongsResponce => {
-        this.findSongs = findSongsResponce.message.body.track_list; 
-      })
-    } */
+    }
   }); 
 
